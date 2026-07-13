49a38132d30abbcad663c631ed1f34cf5f72f46b
GordonSaito <294706591+GordonSaito@users.noreply.github.com>
2026-07-13 11:24:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.butterfly').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+   el.style.left = (Math.random() * 800) + "px";
+   el.style.top = (Math.random() * 400) + "px";
+  });
+});
+
+document.querySelectorAll('.foo').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+   el.style.left = (Math.random() * 800) + "px";
+   el.style.top = (Math.random() * 400) + "px";
+  });
+});
